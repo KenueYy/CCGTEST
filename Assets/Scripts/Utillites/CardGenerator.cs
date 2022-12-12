3cbd80dbef16808caea302e5bc2ebaea8ce049dc
KenueY <kenueyy@yandex.ru>
2022-12-13 00:09:26 +0700

    ready

---
+++ Assets/Scripts/Utillites/CardGenerator.cs
@@ -0,0 +1,21 @@
+﻿using Cards;
+using UnityEngine;
+
+namespace Utillites {
+    public static class CardGenerator {
+        public static CardObject Generate() {
+            CardObject card = new CardObject() {
+                Title = $"Карта {RandomValue()}",
+                Description = $"Наносит {RandomValue()} урона",
+                Manacost = RandomValue(),
+                Attack = RandomValue(),
+                Health = RandomValue(),
+            };
+            return card;
+        }
+        private static int RandomValue() {
+            int value = Random.Range(1, 10);
+            return value;
+        }
+    }
+}
