8c71f16922f44eb804c06089440407e4a8e45949
KenueY <kenueyy@yandex.ru>
2022-12-12 16:59:26 +0700

    fix

--- Assets/Scripts/Cards/CardObject.cs
+++ Assets/Scripts/Cards/CardObject.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-namespace Card {
+namespace Cards {
 
     [CreateAssetMenu(menuName = "CreateCard")]
     public class CardObject : ScriptableObject {
