34d70e9f57efc98f3537a74ada4242ef742fd90d
KenueY <kenueyy@yandex.ru>
2022-12-13 00:15:02 +0700

    Add CardDrag

---
+++ Assets/Scripts/Utillites/MousePosition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utillites {
+    public class MousePosition : MonoBehaviour {
+
+        public static Vector3 GetPointerPosition2D() {
+            Vector3 vector = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            vector.z = 0;
+            return vector;
+        }
+
+        public static Vector2 GetPointerPositionOnWorldPoint(Vector3 position) {
+            Vector2 vector = Camera.main.ScreenToWorldPoint(position);
+            return vector;
+        }
+    }
+}
+
