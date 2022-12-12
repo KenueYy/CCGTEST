d38f6f4bb3f9d7958c006d20150da0de9f686fee
KenueY <kenueyy@yandex.ru>
2022-12-12 16:52:45 +0700

    fix namespace

--- Assets/Scripts/Card/CardObject.cs
+++ Assets/Scripts/Card/CardObject.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "CreateCard")]
-public class CardObject : ScriptableObject
-{
-    public int Manacost;
-    public string Title;
-    public string Description;
-    public int Attack;
-    public int Health;
+namespace Card {
+
+    [CreateAssetMenu(menuName = "CreateCard")]
+    public class CardObject : ScriptableObject {
+        public int Manacost;
+        public string Title;
+        public string Description;
+        public int Attack;
+        public int Health;
+    }
 }
--- Assets/Scripts/Utillites/Image/ImageUtils.cs
+++ Assets/Scripts/Utillites/Image/ImageUtils.cs
@@ -1,8 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Utillites.Image {
+
     public static class ImageUtils {
         public static Sprite SpriteFromTexture2D(Texture2D texture) {
             return Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f), 100.0f);
