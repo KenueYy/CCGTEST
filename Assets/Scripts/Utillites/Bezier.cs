08a30e1aa35586efc1c6475657622710534996a9
KenueY <kenueyy@yandex.ru>
2022-12-13 16:23:57 +0700

    add Bezier namespace

--- Assets/Scripts/Utillites/Bezier.cs
+++ Assets/Scripts/Utillites/Bezier.cs
@@ -1,16 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public static class Bezier
-{
-    public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t) {
-        t = Mathf.Clamp01(t);
-        float oneMinusT = 1f - t;
-        return
-            oneMinusT * oneMinusT * oneMinusT * p0 +
-            3f * oneMinusT * oneMinusT * t * p1 +
-            3f * oneMinusT * t * t * p2 +
-            t * t * t * p3;
+namespace Utillites {
+    public static class Bezier {
+        public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t) {
+            t = Mathf.Clamp01(t);
+            float oneMinusT = 1f - t;
+            return
+                oneMinusT * oneMinusT * oneMinusT * p0 +
+                3f * oneMinusT * oneMinusT * t * p1 +
+                3f * oneMinusT * t * t * p2 +
+                t * t * t * p3;
+        }
     }
 }
